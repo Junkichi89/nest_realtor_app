15e25b551d37f44a75bba9d55a4d118884678b80
Jumpei Asanuma <jun64pei89@gmail.com>
2022-06-28 05:17:15 +0900

    aad: schema and push db

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class AppService {}
+
+/**
+ * DBの設定について
+ *
+ * 1) Install Postgres on local machine localhost:5432
+ *
+ * 2) Host Postgres in cloud
+ *
+ * in Heroku.com
+ */
